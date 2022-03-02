efa64aa215da0fd9359685cea55d8d6440caaa0c
Leiko Ravelo <ravelo.leiko@gmail.com>
2022-03-02 22:22:52 +0800

    add tailwind, berry router, styled. Setup skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './layouts/HomePage';
+import ShoppingCart from './layouts/ShoppingCart';
+
+const Container = styled.div``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="growsarisari-store" element={<HomePage />} />
+        <Route path="growsarisari-store/cart" element={<ShoppingCart />} />
+      </Routes>
+    </Container>
   );
 }
 
