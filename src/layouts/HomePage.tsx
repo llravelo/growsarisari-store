61fd925b86a0cdfd6ee5f20201d0338a745aff31
Leiko Ravelo <ravelo.leiko@gmail.com>
2022-03-03 20:25:08 +0800

    homepage base

--- src/layouts/HomePage.tsx
+++ src/layouts/HomePage.tsx
@@ -1,5 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return <Container>HomePage</Container>;
 }
 
 export default HomePage;
