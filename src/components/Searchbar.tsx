5bde8383ca8a687d27e8657bbf10c07cdc9827af
Leiko Ravelo <ravelo.leiko@gmail.com>
2022-03-06 20:38:06 +0800

    add enter key handler

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -17,6 +17,11 @@ function Searchbar({ searchKey, setSearchKey, onSearch }: SearchbarProps) {
         aria-describedby="button-addon2"
         value={searchKey}
         onChange={(e) => setSearchKey(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onSearch(searchKey);
+          }
+        }}
       />
       <button
         className="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-r shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out items-center"
