0f9d6c31cf814da3b8ab42ae174e90faee8588b2
Leiko Ravelo <ravelo.leiko@gmail.com>
2022-03-06 22:45:22 +0800

    update app css

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ function App() {
   const [page, setPage] = useState<Pages>(Pages.HOME_PAGE);
 
   return (
-    <div className="h-screen w-screen flex flex-col">
+    <div className="h-screen w-screen flex flex-col bg-gray-100">
       <Navbar setPage={setPage} />
       <ProductContext.Provider value={ProductState}>
-        <div className="h-full flex flex-col p-6 bg-gray-100">
+        <div className="h-full flex flex-col p-6 overflow-y-scroll">
           {page === Pages.HOME_PAGE && <HomePage />}
           {page === Pages.CART_PAGE && <ShoppingCart />}
         </div>
